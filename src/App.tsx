34e292b467a8d42fca02eb6372d5f7e163da619f
Artur Kondratiev <akondra@softserveinc.com>
2023-11-21 21:51:21 +0200

    added tsconfig and setted up the project

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,22 @@ import {
   Routes
 } from 'react-router-dom';
 
-
 import NavBar from './components/NavBar/NavBar';
 import CustomTable from './components/Table/CustomTable';
 import Home from './components/Home/Home';
 
 import './App.css';
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={ <Home />} />
-        <Route path="/table" element={ <CustomTable />} />
-      </Routes>
-    </Router>
-  )
+    <NavBar />
+    <Routes>
+      <Route path="/" element={ <Home />} />
+      <Route path="/table" element={ <CustomTable />} />
+    </Routes>
+  </Router>
+  );
 }
+
+export default App;
